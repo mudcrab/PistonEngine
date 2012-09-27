db14d264496c7d95b014a6dc56547a68f8bced9b
Jevgeni Krais <beichu@gmail.com>
2012-09-27 13:27:41 +0300

    draw and update from game method sprite class

--- piston.js
+++ piston.js
@@ -1,25 +1,57 @@
 function Piston(canvas_id, w, h) {
-    this.canvasID = canvas_id;
-    this.graphics;
-    this.canvas;
-    this.context;
-    this.init();
+    this.canvas = {
+        id : canvas_id,
+        c : null,
+        ctx : null,
+        width : w,
+        height : h
+    };
+    this.mainMethod;
+    this.drawArray = null;
 };
-Piston.prototype.init = function()
+Piston.prototype.start = function(class_)
 {
-    this.canvas = document.getElementById(this.canvasID);
-    this.context = this.canvas.getContext('2d');
-    this.context.width = 1440;
-    this.context.height = 775;
+    this.canvas.c = document.getElementById(this.canvas.id);
+    this.canvas.ctx = this.canvas.c.getContext('2d');
+    this.canvas.ctx.width = this.canvas.width;
+    this.canvas.ctx.height = this.canvas.height;
+    this.mainMethod = new class_();
+    this.gameLoop();
 };
 Piston.prototype.draw = function()
 {
-    
+    // clear canvas
+    this.canvas.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.mainMethod.draw();
+};
+Piston.prototype.test = function()
+{
+    console.log('testpiston');
 };
 Piston.prototype.update = function()
 {
+    this.mainMethod.update();
+};
+Piston.prototype.gameLoop = function()
+{
     var that = this;
     setInterval(function() {
-        that.draw();
+        that.update();
     }, 1000 / 60);
+};
+Piston.Sprite = function Sprite(img, x, y)
+{
+    this.image = new Image();
+    this.image.src = img;
+    this.image.x = x;
+    this.image.y = y;
+    return this.image;
+};
+Piston.Sprite.prototype.test = function()
+{
+    console.log('test');
+}
+Piston.Sprite.prototype.draw = function()
+{
+    this.canvas.ctx.drawImage(this.image);
 };
