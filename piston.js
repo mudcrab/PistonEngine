2dec4d823123c0805400461186e9576f37646626
Jevgeni Krais <beichu@gmail.com>
2012-09-28 13:01:28 +0300

    engine converted to prototype.js gameloop now uses requestAnimationFrame (only chrome for now)

--- piston.js
+++ piston.js
@@ -1,57 +1,76 @@
-function Piston(canvas_id, w, h) {
-    this.canvas = {
-        id : canvas_id,
-        c : null,
-        ctx : null,
-        width : w,
-        height : h
-    };
-    this.mainMethod;
-    this.drawArray = null;
-};
-Piston.prototype.start = function(class_)
-{
-    this.canvas.c = document.getElementById(this.canvas.id);
-    this.canvas.ctx = this.canvas.c.getContext('2d');
-    this.canvas.ctx.width = this.canvas.width;
-    this.canvas.ctx.height = this.canvas.height;
-    this.mainMethod = new class_();
-    this.gameLoop();
-};
-Piston.prototype.draw = function()
-{
-    // clear canvas
-    this.canvas.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.mainMethod.draw();
-};
-Piston.prototype.test = function()
-{
-    console.log('testpiston');
-};
-Piston.prototype.update = function()
-{
-    this.mainMethod.update();
-};
-Piston.prototype.gameLoop = function()
-{
-    var that = this;
-    setInterval(function() {
-        that.update();
-    }, 1000 / 60);
-};
-Piston.Sprite = function Sprite(img, x, y)
-{
-    this.image = new Image();
-    this.image.src = img;
-    this.image.x = x;
-    this.image.y = y;
-    return this.image;
-};
-Piston.Sprite.prototype.test = function()
-{
-    console.log('test');
-}
-Piston.Sprite.prototype.draw = function()
-{
-    this.canvas.ctx.drawImage(this.image);
-};
+var PistonEngine = Class.create({
+    canvas : null,
+    ctx : null,
+    canvasWidth : null,
+    canvasHeight : null,
+    mainClass : null,
+    animationFrame : null,
+    img: null,
+    fps: 0,
+    lastCall: null,
+    initialize: function(canvasID, w, h, _mainClass)
+    {
+        
+        canvas = document.getElementById(canvasID);
+        canvasWidth = w;
+        canvasHeight = h;
+        canvas.width = w;
+        canvas.height = h;
+        mainClass = new _mainClass;
+        ctx = canvas.getContext('2d');
+        img = new Image();
+        img.src = 'assets/tiles/player.png';
+        var that = this;
+        // gameloop stuff
+        var animationFrame = window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            null ;
+        if(animationFrame !== null)
+        {
+            if(that.lastCall == null)
+            {
+                that.lastCall = new Date().getTime();
+                that.fps = 0;
+            }
+            var animation = function()
+            {
+                that.loop();
+                setTimeout(function() {
+                    animationFrame(animation, canvas);
+                }, 10)
+                var delta = (new Date().getTime() - that.lastCall) / 1000;
+                that.lastCall = new Date().getTime();
+                that.fps = Math.floor(1 / delta);
+            };
+            animationFrame(animation, canvas);
+        }
+    },
+    draw: function()
+    {
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        var time = new Date().getTime() * 0.002;   
+        var x = Math.sin( time ) * 96 + 128;   
+        var y = Math.cos( time * 0.9 ) * 96 + 128;   
+        var colors = ["red", "orange", "yellow", "green", "blue", "cyan", "purple"];   
+        ctx.fillStyle = 'rgb(245,245,245)';   
+        ctx.fillRect( 0, 0, 1440, 775);
+        ctx.drawImage(img, Math.floor(Math.random() * 1440), Math.floor(Math.random() * 775));
+    },
+    update: function(mainMethod)
+    {
+        
+    },
+    loop: function()
+    {
+        var that = this;
+        jQuery('#framecounter').html(that.fps);
+        that.draw();
+    },
+    stage: function()
+    {
+        
+    }
+});
