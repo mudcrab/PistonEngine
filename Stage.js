c90efcb4badc7c146e22b1ee05a092a0c8ee1ee2
Jevgeni Krais <beichu@gmail.com>
2012-10-03 15:27:37 +0300

    added get, add and remove methods to Stage class

--- Stage.js
+++ Stage.js
@@ -12,13 +12,20 @@ var Stage = Class.create(PistonEngine, {
         var newLength = this.entities.push(entity);
         return newLength - 1; // eg element index
     },
+    addChildAt: function(index, entity)
+    {
+        if(this.entities[index] == undefined)
+        {
+            entities[index] = entity;
+        }
+    },
     removeChild: function(entity)
     {
-        
+        this.entities.splice(this.searchForEntity(entity), 1);
     },
     removeChildAt: function(index)
     {
-        
+        this.entities.splice(index, 1);
     },
     render: function()
     {
@@ -39,8 +46,24 @@ var Stage = Class.create(PistonEngine, {
     {
         return this.entities;
     },
-    getEntity: function(index)
+    getEntity: function(entity)
+    {
+        return this.entities[this.searchForEntity(entity)];
+    },
+    getEntityAt: function(index)
     {
         return this.entities[index];
+    },
+    searchForEntity: function(instanceName)
+    {
+        var index = null;
+        for(var i = 0; i < this.entities.length; i++)
+        {
+            if(this.entities[i].instanceName == instanceName)
+            {
+                index = i;
+            }
+        }
+        return index;
     }
 });
