b63fc84158d9e3a6938bef22c9885bcb91920a2f
Jevgeni Krais <beichu@gmail.com>
2013-04-22 22:59:15 +0300

    task #16, improved debugger class, added an infobar, further improvements can be made in 0.2 milestone

--- debug.js
+++ debug.js
@@ -2,10 +2,21 @@ var PistonDebug = Class.create({
 
 	buffer: [],
 	bufferSize: 500,
-	enabled: true,
+	enabled: false,
+	mstick: 0,
+	barVisible: false,
 
 	initialize: function() {
-
+		var barHTML = '<div id="debug_bar">\
+			<div class="db_item" id="db_fps">FPS 60</div>\
+			<div class="db_item" id="db_time">TIME 60</div>\
+			<div class="db_item" id="db_drawn" title="Total drawn entities current frame">DRAWN 60</div>\
+			<div class="db_item" id="db_total" title="Total entities added to stage">TOTAL 60</div>\
+			<div class="db_item" id="db_log"></div>\
+			<div class="dbclear"></div>\
+		</div>';
+		$(document.body).insert(barHTML);
+		this.hideDebugBar();
 	},
 	log: function() {
 		var date = new Date();
@@ -46,7 +57,36 @@ var PistonDebug = Class.create({
 		}
 		if(this.buffer.length === 0)
 			console.log('Buffer empty');
-	}
+	},
+	showDebugBar: function()
+	{
+		$('debug_bar').show();
+		this.barVisible = true;
+	},
+	hideDebugBar: function()
+	{
+		$('debug_bar').hide();
+		this.barVisible = false;
+	},
+	setBarPosition: function(position) // top or bottom
+	{
 
+	},
+	update: function()
+	{
+		if(this.barVisible)
+		{
+			$('db_fps').update(piston.core.fps + " FPS");
+			$('db_total').update(piston.core.totalEntities + " TOTAL");
+			$('db_drawn').update(piston.core.totalDrawnEntities + " DRAWN");
+			if(this.mstick == 60)
+			{
+				$('db_time').update(piston.core.delta * 1000 + " MS");
+				this.mstick = 0;
+			}
+			else
+				this.mstick++;
+		}
+	}
 });
 piston.debug = new PistonDebug()
