e7ca51429c59f721653ca120b0c3e8f8e562d929
Jevgeni Krais <beichu@gmail.com>
2013-09-23 21:58:54 +0300

    fixed tiledmap class

--- tiledmap.js
+++ tiledmap.js
@@ -16,54 +16,49 @@ PistonTiledMap.prototype.parseTiled = function(mapType, cb)
 {
 	var self = this;
 	var data = {};
-	//$.ajax({ url: 'assets/' + this.file }).done(function(data){
-	/*$.get('assets/' + this.file, function(data) {
-		console.log(data);
-		//onSuccess: function(response) {
-			self.raw = data;
-			self.mWidth = self.raw.width;
-			self.mHeight = self.raw.height;
-			data.width = self.raw.width;
-			data.height = self.raw.height;
-			data.layers = [];
-			console.log(self.raw)
-			for(var l = 0; l < self.raw.layers.length; l++)
+	$.get('assets/' + this.file, function(json) {
+		self.raw = json;
+		self.mWidth = self.raw.width;
+		self.mHeight = self.raw.height;
+		data.width = self.raw.width;
+		data.height = self.raw.height;
+		data.layers = [];
+		for(var l = 0; l < self.raw.layers.length; l++)
+		{
+			if(typeof self.raw.layers[l].data == 'object')
 			{
-				console.log('a')
-				if(typeof self.raw.layers[l].data == 'object')
+				var layer = new Array(data.height);
+				for(var tempX = 0; tempX < data.height; tempX++)
 				{
-					var layer = new Array(data.height);
-					for(var tempX = 0; tempX < data.height; tempX++)
-					{
-						layer[tempX] = new Array(data.width);
-					}
-					var index = 0;
-					for(var _y = 0; _y < data.height; _y++)
-					{
-						for(var _x = 0; _x < data.width; _x++)
-						{
-							var id = self.raw.layers[l].data[index] - 1;
-							if(mapType == 'orthogonal')
-							{
-								var xy = { x: _x * self.raw.tilewidth, y: _y * self.raw.tileheight, lastx: _x * self.raw.tilewidth, lasty: _y * self.raw.tileheight };
-							}
-							else if(mapType == 'isometric')
-							{
-								var xy = {x: (self.raw.tilewidth * _x) + ( (_y % 2 == 1) ? self.raw.tilewidth / 2 : 0 ) - self.raw.tilewidth / 2, y: (self.raw.tileheight / 2) * _y - self.raw.tileheight / 2};
-							}
-
-							var tile = new PistonEntity(xy, { w: self.raw.tilewidth, h: self.raw.tileheight }, self.raw.tilesets[0].tileproperties[id].instanceName);
-							tile.scrollable = true;
-            				tile.clickable = true;
-							layer[_y][_x] = tile;
-							index++;
-						}
-					}
-					data.layers.push(layer);
+					layer[tempX] = new Array(data.width);
 				}
-			}
+				var index = 0;
+				for(var _y = 0; _y < data.height; _y++)
+				{
 
-			cb(data);
-		//}
-	});*/
+					for(var _x = 0; _x < data.width; _x++)
+					{
+						var id = self.raw.layers[l].data[index] - 1;
+						if(mapType == 'orthogonal')
+						{
+							var xy = { x: _x * self.raw.tilewidth, y: _y * self.raw.tileheight, lastx: _x * self.raw.tilewidth, lasty: _y * self.raw.tileheight };
+						}
+						else if(mapType == 'isometric')
+						{
+							var xy = {x: (self.raw.tilewidth * _x) + ( (_y % 2 == 1) ? self.raw.tilewidth / 2 : 0 ) - self.raw.tilewidth / 2, y: (self.raw.tileheight / 2) * _y - self.raw.tileheight / 2};
+						}
+
+						var tile = new PistonEntity(xy, { w: self.raw.tilewidth, h: self.raw.tileheight }, self.raw.tilesets[0].tileproperties[id].instanceName);
+						tile.scrollable = true;
+						tile.clickable = true;
+						layer[_y][_x] = tile;
+						index++;
+					}
+				}
+				data.layers.push(layer);
+				console.log(layer)
+			}
+		}
+		cb(data);
+	})
 };
